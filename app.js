6e7a76e80f6d29b5655845c980489e1d07c637b5
Stefan Pacevitch <stefanpacevitch@stefanpitchsmbp.home>
2016-01-09 11:51:52 +0000

    Added mongodb, now pulling from db"

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/turbineservice');
+
 app.use(express.static(__dirname + '/bower_components'));  
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
@@ -24,9 +29,12 @@ var dData = function() {
 };
 
 setInterval(function() {
-  
-   var newData = { value: dData(), date: new Date() };
 
-   io.emit('data', newData);
+  var newData;
+  var collection = db.get('data');
+
+  collection.findOne({}, function(e,docs){
+        io.emit('data', docs);
+   });
 
 }, 3000);
